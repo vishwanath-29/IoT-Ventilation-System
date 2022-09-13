7a4c1ba2882a2ffc4907c18008d8b1bd36ce9b96
Vishwa <vishwanathnarayanan29@gmail.com>
2022-09-14 00:52:53 +0530

    V1.0.0

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 import time
 import board
 import adafruit_dht
@@ -8,20 +5,13 @@ import adafruit_dht
 # Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT11(board.D4)
 
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
-
 while True:
     try:
-        # Print the values to the serial port
         temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
         print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
+            "Temp:{:.1f} C    Humidity: {}% ".format(
+                 temperature_c, humidity
             )
         )
 
